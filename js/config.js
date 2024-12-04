e57d167bb63adcf339a45572584fb33478e37be2
Aryahajare <162273910+Aryahajare@users.noreply.github.com>
2024-12-04 17:46:16 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_i4mO6RIeT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '70pq5g7mcufvhtu7e0qh8as020', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
